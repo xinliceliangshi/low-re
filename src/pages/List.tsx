18488072d31871e2b7ad5139ac2c24084b49c4f1
wangying <wangying@stargraph.cn>
2023-11-16 16:48:39 +0800

    feat: 列表页

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,8 +1,43 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import styles from './List.module.scss'
+import QuestionCard from '../components/QesionCard'
+const rowQuestion = [
+  {
+    _id: 'q1',
+    title: '问卷1',
+    isPublished: true,
+    isStar: false,
+    answerCount: 0,
+    createdAt: '2020-11-01',
+    updatedAt: new Date(),
+  },
+  {
+    _id: 'q2',
+    title: '问卷2',
+    isPublished: false,
+    isStar: true,
+    answerCount: 5,
+    createdAt: '2020-11-01',
+    updatedAt: new Date(),
+  },
+]
 const List: FC = () => {
+  const [qestionList, setQestionList] = useState(rowQuestion)
   return (
     <>
-      <p>List</p>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <h3>我的问卷</h3>
+        </div>
+        <div className={styles.right}>（搜索）</div>
+      </div>
+      <div className={styles.content}>
+        {qestionList.map(q => {
+          const { _id } = q
+          return <QuestionCard key={_id} {...q} />
+        })}
+      </div>
+      <div className={styles.footer}>底部</div>
     </>
   )
 }
