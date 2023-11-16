d864670062eedbf315607657ed6144296491b23c
wangying <wangying@stargraph.cn>
2023-11-16 15:41:15 +0800

    feat: 建立项目初始化

---
+++ src/components/QesionCard.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+const QuestionCrd: FC = () => {
+  return (
+    <>
+      <p>QuestionCrd</p>
+    </>
+  )
+}
+export default List
---
+++ src/pages/List.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+const List: FC = () => {
+  return (
+    <>
+      <p>List</p>
+    </>
+  )
+}
+export default List
