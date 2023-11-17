b43e79dd2e9c0dee5fb5975cc27d6c8239e33a4f
wangying <wangying@stargraph.cn>
2023-11-17 14:56:08 +0800

    feat: 布局

--- src/pages/Layouts/MainLayouts.tsx
+++ src/pages/Layouts/MainLayouts.tsx
@@ -1,15 +1,22 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
-// import styles from './MainLayout.module.scss'
-const MainLayouts: FC = () => {
+import { Layout, Space } from 'antd'
+const { Header, Footer, Sider, Content } = Layout
+import styles from './MainLayout.module.scss'
+const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
-        <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>Logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Layout className={styles.main}>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+      <Footer className={styles.footer}>小慕问卷 &copy; 2023-present</Footer>
+    </Layout>
   )
 }
-export default MainLayouts
+export default MainLayout
--- src/pages/Layouts/ManagerLayout.tsx
+++ src/pages/Layouts/ManagerLayout.tsx
@@ -1,17 +1,45 @@
 import React, { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
+import { Button, Space, Divider } from 'antd'
+import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
 import styles from './ManagerLayout.module.scss'
 const ManagerLayout: FC = () => {
+  const nav = useNavigate()
+  const { pathname } = useLocation()
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <p>ManagerLayout left</p>
-        <button>创建问卷</button>
-        <br />
-        <a href="#">我的问卷</a>
-        <br />
-        <a href="#">创建问卷</a>
-        <a href="#">回收站</a>
+        <Space direction="vertical" size="large">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            创建问卷
+          </Button>
+          <Divider style={{ borderTop: 'transparent' }} />
+          <Button
+            type="primary"
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => nav('/manager/list')}
+          >
+            我的问卷
+          </Button>
+          <Button
+            type="primary"
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => nav('/manager/star')}
+          >
+            收藏问卷
+          </Button>
+          <Button
+            type="primary"
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => nav('/manager/trash')}
+          >
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
