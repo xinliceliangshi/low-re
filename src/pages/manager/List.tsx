ea1540a3d5ffa574117e2611ad6c2acf4917548a
wangying <wangying@stargraph.cn>
2023-11-29 10:25:57 +0800

    feat: 添加无限滚动，添加防抖优化，添加缓存

--- src/pages/manager/List.tsx
+++ src/pages/manager/List.tsx
@@ -1,13 +1,15 @@
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC, useState, useEffect, useRef, useMemo } from 'react'
 import styles from './common.module.scss'
 import QuestionCard from '../../components/QesionCard'
-import { Typography, Spin } from 'antd'
-import { useTitle } from 'ahooks'
+import { Typography, Spin, Empty } from 'antd'
+import { useDebounceFn, useTitle } from 'ahooks'
 import ListSearch from '../../components/ListSearch'
 import { useRequest } from 'ahooks'
 import { useSearchParams } from 'react-router-dom'
 import { getQuestionListService } from '../../services/question'
 import useLoadQuestionListData from '../../hooks/useLoadQuestionListData'
+import { LIST_PAGE_PARAM_KEY, LIST_PAGE_SIZE, LIST_SEARCH_PARAM_KEY } from '../../constance'
+import { set } from 'react-hook-form'
 // const rowQuestion = [
 //   {
 //     _id: 'q1',
@@ -31,13 +33,76 @@ import useLoadQuestionListData from '../../hooks/useLoadQuestionListData'
 const { Title } = Typography
 const List: FC = () => {
   useTitle('我的问卷')
-  // const [total, setTotal] = useState(0)
-  // const [list, setList] = useState([])
-  const { data = {}, loading } = useLoadQuestionListData()
-  const { list = [], total = 0 } = data
-  console.log(data, 'lllll')
-  // const [searchParams] = useSearchParams()
-  // const [qestionList, setQestionList] = useState(rowQuestion)
+  const [started, setStarted] = useState(false)
+  const [page, setPage] = useState(1)
+  const [list, setList] = useState([])
+  const [total, setTotal] = useState(0)
+  const haveMoreData = total > list.length
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [searchParams] = useSearchParams()
+  const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+
+  useEffect(() => {
+    setStarted(false)
+    setPage(1)
+    setList([])
+    setTotal(0)
+  }, [keyword])
+
+  const { run: load, loading } = useRequest(
+    async () => {
+      const data = await getQuestionListService({
+        page,
+        pageSize: LIST_PAGE_SIZE,
+        keyword: searchParams.get(LIST_PAGE_PARAM_KEY) || '',
+      })
+      console.log(data, 'data')
+      return data
+    },
+    {
+      manual: true,
+      onSuccess(res) {
+        const { list: l = [], total = 0 } = res
+        setList(list.concat(l))
+        setTotal(total)
+        setPage(page + 1)
+      },
+    }
+  )
+  const { run: tryLoadMore } = useDebounceFn(
+    () => {
+      const elem = containerRef.current
+      if (elem == null) return
+      const domRect = elem.getBoundingClientRect()
+      if (domRect == null) return
+      const { bottom } = domRect
+      if (bottom <= document.body.clientHeight) {
+        // console.log('load')
+        load()
+        setStarted(true)
+      }
+    },
+    {
+      wait: 1000,
+    }
+  )
+  const LoadMoreConentElem = useMemo(() => {
+    if (!started) return <Spin />
+    if (total === 0) return <Empty description="暂无数据" />
+    if (!haveMoreData) return <div>没有更多数据了</div>
+    return <div>开始加载下一页</div>
+  }, [started, haveMoreData])
+  useEffect(() => {
+    tryLoadMore()
+  }, [searchParams])
+  useEffect(() => {
+    if (haveMoreData) {
+      window.addEventListener('scroll', tryLoadMore)
+    }
+    return () => {
+      window.removeEventListener('scroll', tryLoadMore)
+    }
+  }, [searchParams, haveMoreData])
   return (
     <>
       <div className={styles.header}>
@@ -49,18 +114,16 @@ const List: FC = () => {
         </div>
       </div>
       <div className={styles.content}>
-        {loading && (
-          <div>
-            <Spin />
-          </div>
-        )}
+        {/* <div style={{ height: '2000px' }}></div> */}
         {list.length > 0 &&
           list.map((q: any) => {
             const { _id } = q
             return <QuestionCard key={_id} {...q} />
           })}
       </div>
-      <div className={styles.footer}>loadMore... 上划加载更多...</div>
+      <div className={styles.footer}>
+        <div ref={containerRef}>{LoadMoreConentElem}</div>
+      </div>
     </>
   )
 }
